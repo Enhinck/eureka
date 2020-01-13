a7d3d5c3efe74a20fcaded1b3bb9bfc6098dc755
huenbin <huenbin@foxmail.com>
2020-01-13 17:32:41 +0800

    修改

--- src/main/java/com/enhinck/eureka/EurekaServiceApplication.java
+++ src/main/java/com/enhinck/eureka/EurekaServiceApplication.java
@@ -1,4 +1,4 @@
-package com.greentown.eureka;
+package com.enhinck.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
